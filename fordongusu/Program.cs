c1f38b86566b0d60a9b73eaf459eae4ac5506e44
volkan_salli <volkan_salli@hotmail.com>
2021-11-12 17:25:40 +0300

    Add files via upload

---
+++ fordongusu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fordongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0, tektop=0, cifttop=0, tumtop=0;
+            Console.WriteLine("1 den 1000 e kadar olan tek ve çift sayıların kendi içinde toplamı");
+            for(i=0;i<=1000;i++){
+                if(i%2==0)
+                cifttop+=i;
+                else
+                tektop+=i;
+            }
+            tumtop=tektop+cifttop;
+            Console.WriteLine("tektoplam=   ");
+            Console.WriteLine(tektop);
+            
+            Console.WriteLine("cifttoplam=   ");
+            Console.WriteLine(cifttop);
+            Console.WriteLine("tumtoplam=   ");
+            Console.WriteLine(tumtop);
+
+
+
+
+
+        }
+    }
+}
